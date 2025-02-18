370f20cd06e46c8da3eb5fa88ba20e0294b6fd78
SimonaBabut <ianossimona18@gmail.com>
2025-02-18 19:08:22 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import React from 'react';
+import { Profile } from './Profile/Profile';
+import user from '../user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {/* Componenta Profile */}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      
+      {/* Componenta Statistics */}
+      <Statistics title="Upload stats" stats={data} />
+
+      {/* Afișăm lista de prieteni */}
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
